aabe16753f3574f7c5d22d366e4bb9bc0bc90998
Luca Zenari <luca.zenari1@tesco.com>
2019-07-11 00:12:53 +0100

    more steps in the powershell setup

--- src/4-setup-powershell.ps1
+++ src/4-setup-powershell.ps1
@@ -1,9 +1,14 @@
 #screenfetch
 
 #posh-git
+PowerShellGet\Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
 
 #oh-my-posh
+Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
+Install-Module oh-my-posh -Scope CurrentUser
 
 #thefuck
+Invoke-Expression "pip install thefuck"
 
 #copy profile.ps1
+Get-Content .\powershell-resources\powershell_profile.ps1 | Set-Content $PROFILE
