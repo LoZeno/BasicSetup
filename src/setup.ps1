e50e33ebcc333809ce20f98cfc3426f6a1838ab5
Luca Zenari <luca.zenari1@tesco.com>
2019-07-12 16:49:37 +0100

    prettify the Write-Host messages in the setup script

--- src/setup.ps1
+++ src/setup.ps1
@@ -1,15 +1,19 @@
 
-Write-Host "Enabling virtualization and WSL"
+Write-Host "Enabling virtualization and WSL... " -ForegroundColor Yellow -NoNewline
 Invoke-Expression $PSScriptRoot\1-enable-windows-features.ps1
-Write-Host "Installing Chocolatey"
+Write-Host "Done" -ForegroundColor Yellow
+Write-Host "Installing Chocolatey... " -ForegroundColor Yellow -NoNewline
 Invoke-Expression $PSScriptRoot\2-install-chocolatey.ps1
+Write-Host "Done" -ForegroundColor Yellow
 Invoke-Expression "C:\ProgramData\chocolatey\bin\RefreshEnv.cmd"
-Write-Host "Installing software"
+Write-Host "Installing software... " -ForegroundColor Yellow -NoNewline
 Invoke-Expression $PSScriptRoot\3-install-software.ps1
+Write-Host "Done" -ForegroundColor Yellow
 Invoke-Expression "C:\ProgramData\chocolatey\bin\RefreshEnv.cmd"
-Write-Host "Setting up powershell modules and profile"
+Write-Host "Setting up powershell modules and profile" -ForegroundColor Yellow
 Invoke-Expression $PSScriptRoot\4-setup-powershell.ps1
-Write-Host "Setting up powershell CORE modules and profile"
+Write-Host "Setting up powershell CORE modules and profile" -ForegroundColor Yellow -NoNewline
 Start-Process pwsh.exe -Wait -Args "-executionpolicy bypass -command Set-Location \`"$PWD\`"; $PSScriptRoot\4-setup-powershell.ps1;"
-Write-Host "setting up vscode plugins"
+Write-Host "Done" -ForegroundColor Yellow
+Write-Host "setting up vscode plugins" -ForegroundColor Yellow
 Invoke-Expression $PSScriptRoot\5-setup-vscode.ps1
