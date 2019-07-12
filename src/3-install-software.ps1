1f640915ca6cc22240a5045a7f7b434257845fea
Luca Zenari <luca.zenari1@tesco.com>
2019-07-12 12:34:00 +0100

    of course, can't use pwsh.exe before it's installed. Reverted to powershell.exe

--- src/3-install-software.ps1
+++ src/3-install-software.ps1
@@ -1,3 +1,3 @@
 $args = "choco install .\chocolatey\packages.config -y"
     
-Start-Process pwsh.exe -Verb RunAs -Args "-executionpolicy bypass -command Set-Location \`"$PWD\`"; $args;"
+Start-Process powershell.exe -Verb RunAs -Args "-executionpolicy bypass -command Set-Location \`"$PWD\`"; $args;"
