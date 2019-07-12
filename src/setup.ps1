78ebf496897f109810a81bf66f82bc0cd7c83817
Luca Zenari <luca.zenari1@tesco.com>
2019-07-12 16:01:26 +0100

    added outputs to explain the steps; added setup to normal powershell (not just powershell core)

--- src/setup.ps1
+++ src/setup.ps1
@@ -1,8 +1,15 @@
 
+Write-Host "Enabling virtualization and WSL"
 Invoke-Expression $PSScriptRoot\1-enable-windows-features.ps1
+Write-Host "Installing Chocolatey"
 Invoke-Expression $PSScriptRoot\2-install-chocolatey.ps1
 Invoke-Expression "C:\ProgramData\chocolatey\bin\RefreshEnv.cmd"
+Write-Host "Installing software"
 Invoke-Expression $PSScriptRoot\3-install-software.ps1
 Invoke-Expression "C:\ProgramData\chocolatey\bin\RefreshEnv.cmd"
+Write-Host "Setting up powershell modules and profile"
+Invoke-Expression $PSScriptRoot\4-setup-powershell.ps1
+Write-Host "Setting up powershell CORE modules and profile"
 Start-Process pwsh.exe -Wait -Args "-executionpolicy bypass -command Set-Location \`"$PWD\`"; $PSScriptRoot\4-setup-powershell.ps1;"
+Write-Host "setting up vscode plugins"
 Invoke-Expression $PSScriptRoot\5-setup-vscode.ps1
