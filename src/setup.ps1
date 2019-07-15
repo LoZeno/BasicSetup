0126c6ad5c755dcfb93f4ee96ce6f770a8b1fd49
Luca Zenari <luca.zenari1@tesco.com>
2019-07-15 09:11:09 +0100

    fixed refreshenv problem

--- src/setup.ps1
+++ src/setup.ps1
@@ -1,19 +1,26 @@
-
 Write-Host "Enabling virtualization and WSL... " -ForegroundColor Yellow -NoNewline
 Invoke-Expression $PSScriptRoot\1-enable-windows-features.ps1
 Write-Host "Done" -ForegroundColor Yellow
+
 Write-Host "Installing Chocolatey... " -ForegroundColor Yellow -NoNewline
 Invoke-Expression $PSScriptRoot\2-install-chocolatey.ps1
 Write-Host "Done" -ForegroundColor Yellow
-Invoke-Expression "C:\ProgramData\chocolatey\bin\RefreshEnv.cmd"
+
+Write-Host "Setting up a refreshenv function alias" -ForegroundColor Yellow
+Import-Module -Name C:\ProgramData\chocolatey\helpers\chocolateyProfile.psm1
+Invoke-Expression "refreshenv"
+
 Write-Host "Installing software... " -ForegroundColor Yellow -NoNewline
 Invoke-Expression $PSScriptRoot\3-install-software.ps1
 Write-Host "Done" -ForegroundColor Yellow
-Invoke-Expression "C:\ProgramData\chocolatey\bin\RefreshEnv.cmd"
-Write-Host "Setting up powershell modules and profile" -ForegroundColor Yellow
-Invoke-Expression $PSScriptRoot\4-setup-powershell.ps1
+
+Invoke-Expression "refreshenv"
+
 Write-Host "Setting up powershell CORE modules and profile" -ForegroundColor Yellow -NoNewline
-Start-Process pwsh.exe -Wait -Args "-executionpolicy bypass -command Set-Location \`"$PWD\`"; $PSScriptRoot\4-setup-powershell.ps1;"
+Start-Process pwsh.exe -Wait -Verb RunAs -Args "-noexit -executionpolicy bypass -command Set-Location \`"$PWD\`"; Install-Module -Name PSReadLine -AllowPrerelease -Force -SkipPublisherCheck"
+Start-Process pwsh.exe -Wait -Args "-noexit -executionpolicy bypass -command Set-Location \`"$PWD\`"; $PSScriptRoot\4-setup-powershell.ps1;"
 Write-Host "Done" -ForegroundColor Yellow
+
 Write-Host "setting up vscode plugins" -ForegroundColor Yellow
 Invoke-Expression $PSScriptRoot\5-setup-vscode.ps1
+Write-Host "Done" -ForegroundColor Yellow
