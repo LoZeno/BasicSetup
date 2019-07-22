b4b8002d951183b07fcac0e7750a803fd5fe44fb
Luca Zenari <luca.zenari1@tesco.com>
2019-07-22 16:33:58 +0100

    ok, fonts should be installed as administrator, so chocolatey is a better candidate for FiraCode

--- src/4-install-scoop.ps1
+++ src/4-install-scoop.ps1
@@ -3,17 +3,16 @@ function installFromScoopList([string]$listName) {
     foreach ($item in Get-Content -Path ".\scoop\scoop-$listName") {
         $scoopCommand = "$scoopCommand $item"
     }
-    Write-Host $scoopCommand
+    Write-Host $scoopCommand -ForegroundColor Yellow
     Invoke-Expression "$scoopCommand"
 }
 
 installFromScoopList "prerequisites"
 
-Write-Host "Adding scoop buckets: extra, java, jetbrains, nerd-fonts" -ForegroundColor Yellow
+Write-Host "Adding scoop buckets: extra, java, jetbrains" -ForegroundColor Yellow
 Invoke-Expression "scoop bucket add extras"
 Invoke-Expression "scoop bucket add jetbrains"
 Invoke-Expression "scoop bucket add java"
-Invoke-Expression "scoop bucket add nerd-fonts"
 
 installFromScoopList "list"
 
