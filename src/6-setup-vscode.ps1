4b570277f6a40fd74e45977450ded0c6adbc42ec
Luca Zenari <luca.zenari1@tesco.com>
2019-07-23 10:03:51 +0100

    improved vscode plugins list installation

--- src/6-setup-vscode.ps1
+++ src/6-setup-vscode.ps1
@@ -1,6 +1,10 @@
-code --install-extension ms-vscode.csharp
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension ms-vsliveshare.vsliveshare
-code --install-extension ms-vscode.powershell
-code --install-extension vscode-icons-team.vscode-icons
+function installCodeExtensions([string]$listName) {
+    $scoopCommand = "code --install-extension"
+    foreach ($item in Get-Content -Path ".\vs-code\plugin-$listName") {
+        $scoopCommand = "$scoopCommand $item"
+    }
+    Write-Host $scoopCommand -ForegroundColor Yellow
+    Invoke-Expression "$scoopCommand"
+}
+
+installCodeExtensions "list"
