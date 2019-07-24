a2de73bb973c859563e1bc87201e7873c7af9282
Luca Zenari <luca.zenari1@tesco.com>
2019-07-24 09:33:13 +0100

    added git lg alias

--- src/7-setup-git.ps1
+++ src/7-setup-git.ps1
@@ -8,6 +8,8 @@ Write-Host "Setting up git user email"
 Invoke-Expression "git config --global user.email $email"
 Write-Host "Setting up vscode as git default editor"
 Invoke-Expression "git config --global core.editor 'code --wait'"
+Write-Host "Setting up git lg alias for prettified one-line logs"
+Invoke-Expression "git config --global alias.logline `"log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit`""
 if (('Y', 'y') -contains $generateSSH) {
     Write-Host "Generating a new ssh key using $email as the label"
     Invoke-Expression "ssh-keygen -t rsa -b 4096 -C '$email'"
