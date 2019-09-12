b503cef78a09306964068949c52c08aacc2042d3
Luca Zenari <luca.zenari1@tesco.com>
2019-09-12 13:08:39 +0100

    added enabling of windows sandbox feature

--- src/1-enable-windows-features.ps1
+++ src/1-enable-windows-features.ps1
@@ -1,4 +1,5 @@
 Write-Host "Enabling virtualization and WSL... " -ForegroundColor Yellow -NoNewline
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -All -NoRestart 
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart
+Enable-WindowsOptionalFeature -Online -FeatureName Containers-DisposableClientVM -All -NoRestart
 Write-Host "Done" -ForegroundColor Yellow
