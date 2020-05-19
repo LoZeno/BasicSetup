12454a714582877cee3722eb42d26b0d13ddb7d2
Zenari, Luca <luca.zenari1@tesco.com>
2020-05-19 16:02:25 +0100

    fix for vscode plugins not being installed

--- src/7-setup-vscode.ps1
+++ src/7-setup-vscode.ps1
@@ -1,6 +1,5 @@
-$installCommand = "code --install-extension"
 foreach ($item in Get-Content -Path ".\vscode.txt") {
-    $installCommand = "$installCommand $item"
+    $installCommand = "code --install-extension $item"
+    Write-Host $installCommand -ForegroundColor Yellow
+    Invoke-Expression "$installCommand"
 }
-Write-Host $installCommand -ForegroundColor Yellow
-Invoke-Expression "$installCommand"
