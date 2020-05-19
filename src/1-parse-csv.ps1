ca4c6ab8ddc99d11bcf963510e6ab1a614f87c44
Zenari, Luca <luca.zenari1@tesco.com>
2020-05-19 10:19:31 +0100

    added csv software list and code for parsing and generating list of categories

---
+++ src/1-parse-csv.ps1
@@ -0,0 +1,18 @@
+﻿$options = ("Y", "N", "y", "n")
+
+$stacks = Import-Csv .\softwareList.csv | group -Property List | select Name
+
+$selectedStacks = @()
+$stacks | ForEach-Object -Process {
+    $installStack = ""
+    $stackName = $_.Name
+
+    while (($stackName -ne "base") -and ($options -notcontains $installStack)) {
+        $installStack = Read-Host "Do you want to install the $stackName stack? [Y]es/[N]o" 
+    }
+    if (($stackName -eq "base") -or (('Y','y') -contains $installStack)) {
+        $selectedStacks += $stackName
+    }
+}
+
+Write-Output $selectedStacks
