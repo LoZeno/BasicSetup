1a1ff211c622583568a0a460b57ce4fce71d4ac0
LoZeno <lozeno1982@gmail.com>
2021-12-06 13:38:51 +0000

    installing wsl with the new commandline utility providing a choice of distributions

--- src/3-enable-windows-features.ps1
+++ src/3-enable-windows-features.ps1
@@ -1,5 +1,13 @@
+$windowsMajorVersion = $args[0]
+$windowsBuild = $args[1]
+
 Write-Host "Enabling virtualization and WSL... " -ForegroundColor Yellow -NoNewline
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -All -NoRestart 
+
+# enabling Microsoft-Windows-Subsystem-Linux is necessary only if on Windows 10 version predating build 19041
+# for anything newer, wsl --install is used in setup.ps1
+if (($windowsMajorVersion -lt 11) -and ($windowsBuild -lt 19041)) {
+    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -All -NoRestart 
+}
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart
 Enable-WindowsOptionalFeature -Online -FeatureName Containers-DisposableClientVM -All -NoRestart
 Write-Host "Done" -ForegroundColor Yellow
--- src/setup.ps1
+++ src/setup.ps1
@@ -23,6 +23,8 @@ while ($options -notcontains $installShovel) {
     $installShovel = Read-Host "Do you want to replace Scoop with Shovel? [Y]es/[N]o" 
 }
 
+
+
 # parsing the CSV file and generating packages.config, scoop.txt, vscode.txt
 # according to user preferences. These files will be used by the appropriate installers
 # to install the applications selected.
@@ -43,16 +45,36 @@ Write-Host "Installing software with scoop... " -ForegroundColor Yellow
 Invoke-Expression "$PSScriptRoot\2-install-scoop.ps1 $installShovel"
 Write-Host "Done" -ForegroundColor Yellow
 
+# installing wsl with the new fancy wsl --install for the windows versions that support it
+if (($windowsVersion.Major -ge 11) -or ($windowsVersion.Build -ge 19041)) {
+    Write-Host "This is the list of wsl distributions available (output of wsl --list --online):" -ForegroundColor Yellow
+    # show the list of available wsl distributions:
+    Invoke-Expression "wsl --list --online"
+    do {
+        $selectedDistro = Read-Host "What distribution from the list do you want to install? (default is Ubuntu)"
+        if ($null -eq $selectedDistro){
+            $wslInstallResult = Invoke-Expression "wsl --install"
+        } else {
+            $wslInstallResult =  Invoke-Expression "wsl --install -d $selectedDistro"
+        }
+    } while ($wslInstallResult.StartsWith("Invalid distribution"))
+}
+
+# performing tasks that require elevated permissions (using gsudo to elevate):
+# 1- enabling windows features
+# 2- installing chocolatey
+# 3- installing software with chocolatey from packages.config
 Write-Host "Performing Admin-level tasks... " -ForegroundColor Yellow -NoNewline
 gsudo Set-ExecutionPolicy RemoteSigned
 Unblock-File -Path $PSScriptRoot\3-enable-windows-features.ps1
 Unblock-File -Path $PSScriptRoot\4-install-chocolatey.ps1
 Unblock-File -Path $PSScriptRoot\5-install-software.ps1
-Invoke-Expression "gsudo $PSScriptRoot\3-enable-windows-features.ps1; 
+Invoke-Expression "gsudo $PSScriptRoot\3-enable-windows-features.ps1 $($windowsVersion.Major) $($windowsVersion.Build); 
         gsudo $PSScriptRoot\4-install-chocolatey.ps1;
         gsudo $PSScriptRoot\5-install-software.ps1"
 Write-Host "Done" -ForegroundColor Yellow
 
+# finally setting up the dev environment
 Write-Host "Setting up a refreshenv function alias" -ForegroundColor Yellow
 Import-Module -Name C:\ProgramData\chocolatey\helpers\chocolateyProfile.psm1
 Invoke-Expression "refreshenv"
@@ -69,14 +91,13 @@ Write-Host "Setting up git" -ForegroundColor Yellow
 Invoke-Expression "$PSScriptRoot\8-setup-git.ps1 '$gitUser' $gitEmail $gitGenerateSSH"
 Write-Host "Done" -ForegroundColor Yellow
 
-# cleaning up
-
+# cleaning up - removing temporary files
 Remove-Item -Path "$PSScriptRoot\packages.config"
 Remove-Item -Path "$PSScriptRoot\scoop.txt"
 Remove-Item -Path "$PSScriptRoot\vscode.txt"
 
-# scoop checkup
+# scoop checkup - shows actions to do manually to tweak scoop/shovel
 Write-Host "Checking up status of scoop..." -ForegroundColor White
 Invoke-Expression "scoop checkup"
 
-Write-Host "Please reboot your machine." - -ForegroundColor White
+Write-Host "Please reboot your machine." -ForegroundColor White
